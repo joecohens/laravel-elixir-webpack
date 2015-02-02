31e706dabbeeb2798ef279b174df826f78251876
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-02-02 17:02:06 -0600

    Updated to Laravel Elixir 0.10.6

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
 var gulp = require('gulp'),
     webpack = require('gulp-webpack'),
-    notify = require('gulp-notify');
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    _ = require('underscore'),
     elixir = require('laravel-elixir'),
-     _ = require('underscore'),
-    utilities = require('laravel-elixir/ingredients/helpers/utilities');
+    utilities = require('laravel-elixir/ingredients/commands/Utilities'),
+    notification = require('laravel-elixir/ingredients/commands/Notification');
+
 
 elixir.extend('webpack', function (src, outputDir, options) {
 
-    var config = this,
-        baseDir = config.assetsDir + 'js',
-        defaultOptions;
+var config = this,
+        defaultOptions = {
+            debug:         ! config.production,
+            srcDir:        config.assetsDir + 'js',
+            output:        config.jsOutput
+        };
 
-    defaultOptions = {
-    };
-
-    src = utilities.buildGulpSrc(src, baseDir, '**/*.js');
+    options = _.extend(defaultOptions, options);
+    src = "./" + utilities.buildGulpSrc(src, options.srcDir);
 
     options = _.extend(defaultOptions, options);
 
     gulp.task('webpack', function () {
-        var onError = function(err) {
-            notify.onError({
-                title:    "Laravel Elixir",
-                subtitle: "Webpack Compilation Failed!",
-                message:  "Error: <%= error.message %>",
-                icon: __dirname + '/../laravel-elixir/icons/fail.png'
-            })(err);
 
+        var onError = function(e) {
+            new notification().error(e, 'Webpack Compilation Failed!');
             this.emit('end');
         };
 
         return gulp.src(src)
             .pipe(webpack(options)).on('error', onError)
-            .pipe(gulpif(config.production, uglify()))
-            .pipe(gulp.dest(outputDir || config.jsOutput))
-            .pipe(notify({
-                title: 'Laravel Elixir',
-                subtitle: 'Webpack Compiled!',
-                icon: __dirname + '/../laravel-elixir/icons/laravel.png',
-                message: ' '
-            }));
+            .pipe(gulpIf(! options.debug, uglify()))
+            .pipe(gulp.dest(options.output))
+            .pipe(new notification().message('Webpack Compiled!'));
     });
 
-    this.registerWatcher('webpack', baseDir + '/**/*.js');
+    this.registerWatcher('webpack', options.srcDir + '/**/*.js');
 
     return this.queueTask('webpack');
-
 });

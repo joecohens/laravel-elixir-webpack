9d9546c9cb49d8a59cef599cd91986faf252659c
Joseph Cohen <joseph.cohen@dinkbit.com>
2016-02-10 02:43:49 -0600

    Watch the entire assets folder, fix dependencies, multiple entry points.

--- README.md
+++ README.md
@@ -13,30 +13,59 @@ npm install --save-dev laravel-elixir-webpack
 ### Example *Gulpfile*:
 
 ```javascript
-var elixir = require("laravel-elixir");
+var elixir = require('laravel-elixir');
 
-require("laravel-elixir-webpack");
+require('laravel-elixir-webpack');
 
 elixir(function(mix) {
-    mix.webpack("app.js");
+    mix.webpack('app.js');
 });
-
 ```
-First argument is the entry point of your application _(default directory is resources/assets/js)_. In third argument you could pass webpack options. In production bundle will be compressed.
+
+You can also use multiple entry points.
+
+```javascript
+var elixir = require('laravel-elixir');
+
+require('laravel-elixir-webpack');
+
+elixir(function(mix) {
+    mix.webpack(['app.js', 'backend.js']);
+});
+```
 
 #### Advanced example
 
 ```javascript
 elixir(function(mix) {
-    mix.webpack("app.js", {
-        outputDir: "public/js",
-        entry: {
-            app: "src/app.js",
-            test: "test/test.js",
+    mix.webpack('app.js', {
+        module: {
+          loaders: [
+            { test: /\.css$/, loader: 'style!css' },
+          ],
         },
-        output: {
-            filename: "bundle.js"
-        }
     });
+});
+```
+
+Multiple entry points.
+
+```javascript
+elixir(function(mix) {
+    mix.webpack(['app.js', 'backend.js'], {
+        module: {
+          loaders: [
+            { test: /\.css$/, loader: 'style!css' },
+          ],
+        },
+    });
+});
+```
+
+Setting an output file.
+
+```javascript
+elixir(function(mix) {
+    mix.webpack('app.js', {}, './your-public-path/app.js');
 });
 ```
--- index.js
+++ index.js
@@ -1,50 +1,54 @@
-var gulp = require('gulp'),
-    webpack = require('webpack-stream'),
-    gulpIgnore = require('gulp-ignore'),
-    _ = require('underscore'),
-    Elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var named = require('vinyl-named');
+var _ = require('underscore');
+var Elixir = require('laravel-elixir');
 
 var $ = Elixir.Plugins;
 var config = Elixir.config;
 
-Elixir.extend('webpack', function (src, options) {
-    options = _.extend({
-        debug:     ! config.production,
-        srcDir:    config.get('assets.js.folder'),
-        outputDir: config.get('public.js.outputFolder'),
-    }, options);
+Elixir.extend('webpack', function (src, options, output) {
+    var paths = prepGulpPaths(src, output);
+
+    if (_.isObject(src) && !_.isArray(src)) {
+        options = src;
+    } else {
+        options = _.extend({
+            output: {
+                filename: paths.output.name,
+            },
+        }, options);
+    }
 
     new Elixir.Task('webpack', function () {
-        var paths = prepGulpPaths(src, options.srcDir, options.outputDir);
-
         this.log(paths.src, paths.output);
 
         return (
             gulp.src(paths.src.path)
+                .pipe($.if(!_.isArray(paths.src.path), named()))
                 .pipe(webpack(options))
                 .on('error', function(e) {
                     new Elixir.Notification('Webpack Compilation Failed!');
 
                     this.emit('end');
                 })
-                .pipe(gulpIgnore.include('*.js'))
-                .pipe($.if(! options.debug, $.uglify()))
+                .pipe($.if(! config.production, $.uglify()))
                 .pipe(gulp.dest(paths.output.baseDir))
                 .pipe(new Elixir.Notification('Webpack Compiled!'))
         );
     })
-    .watch(options.srcDir + '/**/*');
+    .watch(config.assetsPath + '/**/*');
 });
 
 /**
  * Prep the Gulp src and output paths.
  *
  * @param {string|array} src
- * @param {string|null}  baseDir
  * @param {string|null}  output
+ * @return {object}
  */
-var prepGulpPaths = function(src, baseDir, output) {
+var prepGulpPaths = function(src, output) {
     return new Elixir.GulpPaths()
-        .src(src, baseDir)
-        .output(output, 'app.js');
+        .src(src, config.get('assets.js.folder'))
+        .output(output || config.get('public.js.outputFolder'), 'app.js');
 };
